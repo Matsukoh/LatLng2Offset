6694e986ee1574727c271336952b8db903fe20a1
Kohei Matsumoto <matsumoto@irvs.ait.kyushu-u.ac.jp>
2018-09-06 21:47:11 +0900

    Add script

---
+++ generate_offset.py
@@ -0,0 +1,59 @@
+import numpy as np
+import dask.dataframe as ddf
+import sys
+import csv
+
+lat_lng_1 = sys.argv[1]
+lat_lng_2 = sys.argv[2]
+map_data = sys.argv[3]
+map_pos = []
+latlng_pos = []
+earth_radius = 6378.137
+
+df_1 = ddf.read_csv(lat_lng_1)
+df_2 = ddf.read_csv(lat_lng_2)
+
+
+def latlng_to_diff(latlng_pos):
+    diff_x = earth_radius * (np.radians(latlng_pos[0][1]) - np.radians(latlng_pos[1][1])) * 1000
+    diff_y = earth_radius * (np.radians(latlng_pos[0][0]) - np.radians(latlng_pos[1][0])) * 1000
+
+    return [diff_x, diff_y]
+
+
+def calc_offset(latlng_pos, map_pos):
+    dist_latlng = latlng_to_diff(latlng_pos)
+    dist_map = np.array(map_pos[0]) - np.array(map_pos[1])
+    l_latlng = np.linalg.norm(dist_latlng)
+    l_map = np.linalg.norm(dist_map)
+
+    dist_offset = l_map / l_latlng
+    angle_offset = (np.arctan(dist_map[1]/dist_map[0]) - np.arctan(dist_latlng[1] / dist_latlng[0])) * 180 / np.pi
+
+    return dist_offset, angle_offset
+
+
+with open(map_data, 'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+
+    for row in reader:
+        row = [float(s) for s in row]
+        map_pos.append(row)
+
+
+meen_1 = df_1.mean().compute()
+meen_2 = df_2.mean().compute()
+
+latlng_pos.append([meen_1[0], meen_1[1]])
+latlng_pos.append([meen_2[0], meen_2[1]])
+
+dist_offset, angle_offset = calc_offset(latlng_pos, map_pos)
+
+with open("offset.txt", 'w') as f:
+   f.write("Distance Offset\nAngle Offset\n")
+   f.write("------------------------------\n")
+   f.write(str(dist_offset) + "\n")
+   f.write(str(angle_offset) + "\n")
+
+
