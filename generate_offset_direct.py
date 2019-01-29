13c681c1f7d2075517d5dd77a4dee24160e7f797
Kohei Matsumoto <matsumoto@irvs.ait.kyushu-u.ac.jp>
2019-01-29 11:59:49 +0900

    Change projection method

--- generate_offset_direct.py
+++ generate_offset_direct.py
@@ -1,28 +1,22 @@
 import numpy as np
 import dask.dataframe as ddf
 import sys
-import csv
 import pyproj
 import math
+import csv
 
 wgs84 = pyproj.Proj(init='EPSG:4326')
 rect6 = pyproj.Proj(init='EPSG:2444')
 
-lat_lng_1 = sys.argv[1]
-lat_lng_2 = sys.argv[2]
-map_data = sys.argv[3]
+# lat_lng_1 = sys.argv[1]
+# lat_lng_2 = sys.argv[2]
+map_data = sys.argv[1]
 map_pos = []
 latlng_pos = []
 earth_radius = 6378.137
-
-df_1 = ddf.read_csv(lat_lng_1)
-df_2 = ddf.read_csv(lat_lng_2)
-
-# def latlng_to_diff(latlng_pos):
-#     diff_x = earth_radius * (np.radians(latlng_pos[0][1]) - np.radians(latlng_pos[1][1])) * 1000
-#     diff_y = earth_radius * (np.radians(latlng_pos[0][0]) - np.radians(latlng_pos[1][0])) * 1000
 #
-#     return [diff_x, diff_y]
+# df_1 = ddf.read_csv(lat_lng_1)
+# df_2 = ddf.read_csv(lat_lng_2)
 
 
 def latlng_to_diff(latlng_pos):
@@ -37,6 +31,8 @@ def latlng_to_diff(latlng_pos):
 
 
 def calc_offset(latlng_pos, map_pos):
+    print(latlng_pos)
+    print(map_pos)
     dist_latlng = latlng_to_diff(latlng_pos)
     dist_map = np.array(map_pos[0]) - np.array(map_pos[1])
     l_latlng = np.linalg.norm(dist_latlng)
@@ -55,20 +51,25 @@ with open(map_data, 'r') as f:
     for row in reader:
         row = [float(s) for s in row]
         map_pos.append(row)
+#
+#
+# ave_1 = df_1.mean().compute()
+# ave_2 = df_2.mean().compute()
 
+origin_lat = 33.5955422826
+origin_lng = 130.2190076182
+ref_lat = 33.595433
+ref_lng = 130.219152
 
-ave_1 = df_1.mean().compute()
-ave_2 = df_2.mean().compute()
-
-latlng_pos.append([ave_1[0], ave_1[1]])
-latlng_pos.append([ave_2[0], ave_2[1]])
+latlng_pos.append([origin_lat, origin_lng])
+latlng_pos.append([ref_lat, ref_lng])
 
 dist_offset, angle_offset = calc_offset(latlng_pos, map_pos)
 
 print("Distance Offset : " + str(dist_offset))
 print("Angle Offset : " + str(angle_offset))
-print("Average1 : \n" + str(ave_1))
-print("Average2 : \n" + str(ave_2))
+print("Average1 : \n" + str([origin_lat, origin_lng]))
+print("Average2 : \n" + str([ref_lat, ref_lng]))
 
 with open("offset.txt", 'w') as f:
    f.write("Distance Offset\nAngle Offset\n")
@@ -80,8 +81,8 @@ with open("offset.txt", 'w') as f:
    f.write("Average\n")
    f.write("------------------------------\n")
    f.write("lat, lng\n")
-   f.write(str(ave_1[0]) + "," + str(ave_1[1]) + "\n")
-   f.write(str(ave_2[0]) + "," + str(ave_2[1]) + "\n")
+   f.write(str(origin_lat) + "," + str(origin_lng) + "\n")
+   f.write(str(ref_lat) + "," + str(ref_lng) + "\n")
 
 
 
